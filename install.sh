f28d47ba74ff24ebee5f54639ae026b450a76a88
Joshua Chen <iesugrace@gmail.com>
2015-11-11 15:12:21 +0800

    Add collect_info and ask functions
    collect_info() collects information from user interactively,
    ask() function is called from collect_info.

--- install.sh
+++ install.sh
@@ -21,6 +21,35 @@ check_dep() {
     return 0
 }
 
+# prompt to the terminal and get input
+ask() {
+    local INVISIBLE message default input
+    if test "$1" = "-s"; then
+        INVISIBLE="-s"
+        shift
+    fi
+    message=$1
+    default=$2
+    while true
+    do
+        # stdout been captured, we use stderr instead
+        echo -n "$message [$default]: " >&2
+        read $INVISIBLE input
+        test -n "$INVISIBLE" && echo >&2   # output a newline when no-echo
+        if test -z "$input"; then
+            if test -z "$default"; then
+                echo "invalid input" >&2
+                continue
+            else
+                input=$default
+            fi
+        fi
+        echo "$input"
+        return 0
+    done
+}
+
+# collect information from the user and the system
 collect_info() {
     MSMTP=$(which msmtp)
     PROCMAIL=$(which procmail)
@@ -29,8 +58,8 @@ collect_info() {
     USERNAME=$(ask "user name" "$EMAILADDR")
     PASSWORD=$(ask -s "password")
     local domain_name=$(awk -F@ '{print $2}' <<< "$EMAILADDR")
-    SMTPSERVER=$(ask -s "sending server" "smtp.$domain_name")
-    POPSERVER=$(ask -s "receiving server" "pop.$domain_name")
+    SMTPSERVER=$(ask "sending server" "smtp.$domain_name")
+    POPSERVER=$(ask "receiving server" "pop.$domain_name")
 }
 
 DEP="mutt msmtp gpg getmail"
